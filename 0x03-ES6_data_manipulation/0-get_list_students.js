50524e698cf1f0b2cdcb2ec3b15bb14013cf8614
Omo <omoikheloa56@gmail.com>
2024-07-29 13:28:35 +0000

    mand tasks

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const sl1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const sl2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const sl3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [sl1, sl2, sl3];
+}
